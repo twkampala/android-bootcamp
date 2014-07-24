a208e2028ce7f38b83871c089abf3ad258c292f4
andreweskeclarke <andreweskeclarke@gmail.com>
2014-07-24 22:15:47 +0300

    Adding initial ORMLite structure

---
+++ src/main/java/com/tw/kampala/androidbootcamp/database/DatabaseHelper.java
@@ -0,0 +1,32 @@
+package com.tw.kampala.androidbootcamp.database;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import com.j256.ormlite.android.apptools.OrmLiteSqliteOpenHelper;
+import com.j256.ormlite.support.ConnectionSource;
+import com.j256.ormlite.table.TableUtils;
+import com.tw.kampala.androidbootcamp.models.Item;
+
+import java.sql.SQLException;
+
+public class DatabaseHelper extends OrmLiteSqliteOpenHelper {
+
+    private static final String DATABASE_NAME = "bootcamp.db";
+    private static final int DATABASE_VERSION = 1;
+
+    public DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase, ConnectionSource connectionSource) {
+        try {
+            TableUtils.createTable(connectionSource, Item.class);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, ConnectionSource connectionSource, int i, int i2) {}
+}
--- src/main/java/com/tw/kampala/androidbootcamp/models/Item.java
+++ src/main/java/com/tw/kampala/androidbootcamp/models/Item.java
@@ -1,11 +1,90 @@
 package com.tw.kampala.androidbootcamp.models;
 
+import com.j256.ormlite.field.DatabaseField;
+import com.j256.ormlite.table.DatabaseTable;
+
+@DatabaseTable(tableName = "items")
 public class Item {
-    private int id;
+    @DatabaseField(id = true)
+    private Integer id;
+    @DatabaseField
+    private Integer uuid;
+    @DatabaseField
     private String name;
+    @DatabaseField
     private String city;
+    @DatabaseField
     private String company;
+    @DatabaseField
     private String email;
+    @DatabaseField
     private String phone;
+    @DatabaseField
     private String description;
+
+    public Item() {}
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(int uuid) {
+        this.uuid = uuid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
