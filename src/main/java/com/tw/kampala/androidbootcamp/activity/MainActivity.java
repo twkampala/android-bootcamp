e1d5ce1cb019fe13fd859f15794f475bc8945f23
andreweskeclarke <andreweskeclarke@gmail.com>
2014-07-24 22:29:25 +0300

    Access Items DB during activity to trigger db create

--- src/main/java/com/tw/kampala/androidbootcamp/activity/MainActivity.java
+++ src/main/java/com/tw/kampala/androidbootcamp/activity/MainActivity.java
@@ -7,15 +7,24 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
+import com.j256.ormlite.android.apptools.OpenHelperManager;
+import com.j256.ormlite.dao.CloseableWrappedIterable;
+import com.j256.ormlite.dao.Dao;
 import com.tw.kampala.androidbootcamp.R;
+import com.tw.kampala.androidbootcamp.database.DatabaseHelper;
+import com.tw.kampala.androidbootcamp.models.Item;
 import com.tw.kampala.androidbootcamp.service.SyncService;
 import roboguice.activity.RoboActivity;
 import roboguice.inject.InjectView;
+
+import java.sql.SQLException;
+import java.util.Iterator;
 
 public class MainActivity extends RoboActivity {
 
     @InjectView(R.id.sync_button)
     Button syncButton;
+    private DatabaseHelper databaseHelper;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -29,6 +38,8 @@ public class MainActivity extends RoboActivity {
                 startService(intent);
             }
         });
+
+        iterateThroughItems();
     }
 
     @Override
@@ -37,4 +48,31 @@ public class MainActivity extends RoboActivity {
         return true;
     }
 
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (databaseHelper != null) {
+            OpenHelperManager.releaseHelper();
+            databaseHelper = null;
+        }
+    }
+
+    private DatabaseHelper getHelper() {
+        if (databaseHelper == null) {
+            databaseHelper =
+                    OpenHelperManager.getHelper(this, DatabaseHelper.class);
+        }
+        return databaseHelper;
+    }
+
+    private void iterateThroughItems() {
+        try {
+            Dao<Item, Integer> dao = getHelper().getDao(Item.class);
+            CloseableWrappedIterable<Item> wrappedIterable = dao.getWrappedIterable();
+            Iterator<Item> itemIterator = wrappedIterable.iterator();
+            while(itemIterator.hasNext()) {};
+        } catch (SQLException e) {
+            throw(new RuntimeException(e));
+        }
+    }
 }
