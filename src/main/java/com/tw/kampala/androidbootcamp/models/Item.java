a5e90b15a6c7c16be6cb03ed44228eb2043f3b08
andreweskeclarke <andreweskeclarke@gmail.com>
2014-07-24 19:22:53 +0300

    Adding bare Item model

---
+++ src/main/java/com/tw/kampala/androidbootcamp/models/Item.java
@@ -0,0 +1,11 @@
+package com.tw.kampala.androidbootcamp.models;
+
+public class Item {
+    private int id;
+    private String name;
+    private String city;
+    private String company;
+    private String email;
+    private String phone;
+    private String description;
+}
