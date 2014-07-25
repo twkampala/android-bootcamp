bb5605d5444a083a37072b60e9ddc14a20c21714
rdsubhas <rdsubhas@gmail.com>
2014-07-25 20:57:20 +0300

    Added Identicons for returning profile images

--- sync-server/app.rb
+++ sync-server/app.rb
@@ -1,6 +1,7 @@
 require 'sinatra'
 require 'faker'
 require 'json'
+require 'ruby_identicon'
 
 set :bind, '0.0.0.0'
 
@@ -10,21 +11,26 @@ fake_items = (1..20).map do |i|
 end
 items = Hash[*fake_items.flatten]
 
-get '/items/' do
-  sleep 0.5
+get '/items/?' do
+  sleep 0.2
   content_type :json
   { ids: items.keys }.to_json
 end
 
-put '/items/:id' do
-  sleep 0.5
+put '/items/:id/?' do
+  sleep 0.2
   content_type :json
   items[params[:id]] = params[:item]
   200
 end
 
-get '/items/:id' do
-  sleep 0.5
+get '/items/:id/?' do
+  sleep 0.2
   content_type :json
   items[params[:id]].to_json
+end
+
+get '/items/:id/avatar/?' do
+  content_type 'image/jpeg'
+  RubyIdenticon.create params[:id]
 end
